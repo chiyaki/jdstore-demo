b974df805d7feecfbc5e705b21bab7cd62caf372
kay <kaywangfan@163.com>
2017-02-19 18:16:51 +0800

    config.qiniu_up_host

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -26,4 +26,5 @@ CarrierWave.configure do |config|
   config.qiniu_bucket_domain = ENV["qiniu_bucket_domain"]
   config.qiniu_block_size    = 4*1024*1024
   config.qiniu_protocal      = "http"
+  config.qiniu_up_host       = 'http://up.qiniug.com' #七牛上传海外服务器,国内使用可以不要这行配置
 end
