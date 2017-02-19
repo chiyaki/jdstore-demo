7502dba098ecb50dac26c413992bda48e700c241
kay <kaywangfan@163.com>
2017-02-19 15:55:55 +0800

    may be qiniu_protocal

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -25,7 +25,5 @@ CarrierWave.configure do |config|
   config.qiniu_bucket        = "qiniu_bucket"
   config.qiniu_bucket_domain = "qiniu_bucket_domain"
   config.qiniu_block_size    = 4*1024*1024
-  config.qiniu_protocol      = "http"
-
-  config.qiniu_up_host       = 'http://up.qiniug.com' #七牛上传海外服务器,国内使用可以不要这行配置
+  config.qiniu_protocal      = "http"
 end
